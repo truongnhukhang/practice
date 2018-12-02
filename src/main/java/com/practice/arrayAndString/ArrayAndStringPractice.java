ed6d7ede995d5af5ca98e09096f7b680fbebe11d
sicacho <trustme013@gmail.com>
2018-12-02 23:16:14 +0700

    initCOmmit

---
+++ src/main/java/com/practice/arrayAndString/ArrayAndStringPractice.java
@@ -0,0 +1,54 @@
+package com.practice.arrayAndString;
+
+public class ArrayAndStringPractice {
+  public static void main(String[] args) {
+    System.out.println(removeDuplicates2("aavbnnn".toCharArray()));
+    System.out.println(removeDuplicates2("ababa".toCharArray()));
+    System.out.println(removeDuplicates2("aaabnn".toCharArray()));
+    System.out.println(removeDuplicates2("abacabnn".toCharArray()));
+    System.out.println(removeDuplicates2("aaaaaaa".toCharArray()));
+  }
+
+  public static char[] removeDuplicates(char[] str) {
+    if (str == null) return str;
+    if (str.length < 2) return str;
+    int tail = 0;
+    for (int i = 1; i < str.length; i++) {
+      for (int j = 0; j < i; j++) {
+        if(str[j]==str[i]) {
+          break;
+        } else if(str[j]!=str[i] && j == tail) {
+          tail++;
+          str[tail] = str[i];
+          break;
+        }
+      }
+      if(i==str.length-1) {
+        for (int k = tail+1;k < str.length ;k++) {
+          str[k]=0;
+        }
+      }
+    }
+    return str;
+  }
+
+  public static char[] removeDuplicates2(char[] str) {
+    if (str == null) return str;
+    if (str.length < 2) return str;
+    int tail = 1;
+    for (int i = 1; i < str.length; ++i) {
+      int j ;
+      for ( j = 0; j < tail; ++j) {
+        if(str[i]==str[j]) {
+          break;
+        }
+      }
+      if(j==tail) {
+        str[tail] = str[i];
+        ++tail;
+      }
+    }
+    str[tail] = 0;
+    return str;
+  }
+}
