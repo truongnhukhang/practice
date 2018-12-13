1e3a2ab1f181bd2b82cf21e669f19b34bcaa9a07
truongnhukhang <truongnhukhang1993@gmail.com>
2018-12-13 11:22:41 +0700

    keep learning

--- src/main/java/com/practice/arrayAndString/RotateMatrixNxN.java
+++ src/main/java/com/practice/arrayAndString/RotateMatrixNxN.java
@@ -10,7 +10,18 @@ public class RotateMatrixNxN {
     matrix[1] = new String[]{"E", "F", "G", "H"};
     matrix[2] = new String[]{"I", "K", "L", "M"};
     matrix[3] = new String[]{"N", "P", "Q", "T"};
-    matrix = rotate(matrix);
+    matrix = rotateInPlace(matrix);
+    for (int i = 0; i < matrix.length; i++) {
+      for (int j = 0; j < matrix[i].length; j++) {
+        System.out.print(matrix[i][j] + " ");
+      }
+      System.out.println();
+    }
+    matrix = new String[3][3];
+    matrix[0] = new String[]{"A", "B", "C"};
+    matrix[1] = new String[]{"E", "F", "G"};
+    matrix[2] = new String[]{"I", "K", "L"};
+    matrix = rotateInPlace(matrix);
     for (int i = 0; i < matrix.length; i++) {
       for (int j = 0; j < matrix[i].length; j++) {
         System.out.print(matrix[i][j] + " ");
@@ -19,7 +30,7 @@ public class RotateMatrixNxN {
     }
   }
 
-  public static String[][] rotate(String[][] matrixNxN) {
+  public static String[][] rotateOutPlace(String[][] matrixNxN) {
     if(matrixNxN.length < 2)
       return matrixNxN;
     int length = matrixNxN.length;
@@ -31,5 +42,28 @@ public class RotateMatrixNxN {
       }
     }
     return result;
+  }
+
+  public static String[][] rotateInPlace(String[][] matrixNxN) {
+    if(matrixNxN.length < 2)
+      return matrixNxN;
+    int length = matrixNxN.length;
+    int maxIndex = length-1;
+    long middle = Math.round(length*1.0/2);
+    for (int i = 0; i < length-middle; i++) {
+      for (int j = 0; j < middle; j++) {
+        swapPoint(matrixNxN,new int[]{i,j},new int[]{j,maxIndex-i});
+        swapPoint(matrixNxN,new int[]{i,j},new int[]{maxIndex-i,maxIndex-j});
+        swapPoint(matrixNxN,new int[]{i,j},new int[]{maxIndex-j,i});
+
+      }
+    }
+    return matrixNxN;
+  }
+
+  static void swapPoint(String[][] matrixNxN,int[] point1,int[] point2) {
+    String temp = matrixNxN[point1[0]][point1[1]];
+    matrixNxN[point1[0]][point1[1]] = matrixNxN[point2[0]][point2[1]];
+    matrixNxN[point2[0]][point2[1]] = temp;
   }
 }
