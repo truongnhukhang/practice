d92d76c731e0b333b08df0e1a35ce752bfb923be
truongnhukhang <truongnhukhang1993@gmail.com>
2018-12-26 11:15:53 +0700

    keep learning

--- src/main/java/com/practice/bit/PlusTwoNumber.java
+++ src/main/java/com/practice/bit/PlusTwoNumber.java
@@ -1,11 +1,24 @@
 package com.practice.bit;
 
+import java.util.Arrays;
+
 public class PlusTwoNumber {
   public static void main(String[] args) {
-
+    int[] num1 = {1,1,1,1};
+    int[] num2 = {1,1,1,1};
+    Arrays.stream(plusTwoNumber(num1,num2)).forEach(System.out::print);
   }
 
   static int[] plusTwoNumber(int[] num1,int[] num2) {
-
+    int[] result = new int[num1.length+1];
+    int carry = 0;
+    int orginalResultAndCarry;
+    for( int i = num1.length-1;i > -1;i--) {
+      orginalResultAndCarry = num1[i]+num2[i]+carry;
+      result[i+1] = orginalResultAndCarry%2;
+      carry = orginalResultAndCarry/2;
+    }
+    result[0]=carry;
+    return result;
   }
 }
