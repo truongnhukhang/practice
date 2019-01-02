1bd493ee127b5b820b4c570e7b040bed9945b7dc
sicacho <trustme013@gmail.com>
2019-01-02 22:46:03 +0700

    keep learning

---
+++ src/main/java/com/practice/stack/TowerOfHanoi.java
@@ -0,0 +1,13 @@
+package com.practice.stack;
+
+public class TowerOfHanoi {
+  StackImplementSimple<Integer> rods;
+
+  public static void main(String[] args) {
+
+  }
+
+  static void moveDiskToAnotherRods(StackImplementSimple<Integer> rods,StackImplementSimple<Integer> targetRods) {
+
+  }
+}
