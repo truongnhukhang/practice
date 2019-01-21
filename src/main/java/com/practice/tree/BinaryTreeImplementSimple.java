7ddccc38680cc0382b8bbb339dc241b1f42b3146
truongnhukhang <truongnhukhang1993@gmail.com>
2019-01-21 17:03:58 +0700

    keep learning

---
+++ src/main/java/com/practice/tree/BinaryTreeImplementSimple.java
@@ -0,0 +1,9 @@
+package com.practice.tree;
+
+/**
+ * Created by truongnhukhang on 1/21/19.
+ */
+public class BinaryTreeImplementSimple<T> {
+  Node<T> root;
+
+}
---
+++ src/main/java/com/practice/tree/Node.java
@@ -0,0 +1,10 @@
+package com.practice.tree;
+
+/**
+ * Created by truongnhukhang on 1/21/19.
+ */
+public class Node<T> {
+  public T value;
+  public Node<T> leftChild;
+  public Node<T> rightChild;
+}
