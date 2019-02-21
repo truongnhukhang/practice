16bc35199b4a4a4ea68a8fd237c8767038864dfb
truongnhukhang <truongnhukhang1993@gmail.com>
2019-02-21 15:13:19 +0700

    keep learning

---
+++ src/main/java/com/practice/tree/BinarySearchTreeImplementSimple.java
@@ -0,0 +1,33 @@
+package com.practice.tree;
+
+/**
+ * Created by truongnhukhang on 1/21/19.
+ */
+public class BinarySearchTreeImplementSimple<T extends Comparable<T>> {
+  Node<T> root;
+
+  public void insert(T data) {
+    insertToNode(data,root);
+  }
+
+  private void insertToNode(T data,Node<T> node) {
+    if(data.compareTo(node.value)==-1) {
+      if(node.leftChild!=null) {
+        insertToNode(data,node.leftChild);
+      } else {
+        Node<T> insertNode = new Node<>();
+        insertNode.value = data;
+        node.leftChild = insertNode;
+      }
+    } else {
+      if(node.rightChild!=null) {
+        insertToNode(data,node.rightChild);
+      } else {
+        Node<T> insertNode = new Node<>();
+        insertNode.value = data;
+        node.rightChild = insertNode;
+      }
+    }
+  }
+
+}
--- src/main/java/com/practice/tree/BinaryTreeImplementSimple.java
+++
@@ -1,9 +0,0 @@
-package com.practice.tree;
-
-/**
- * Created by truongnhukhang on 1/21/19.
- */
-public class BinaryTreeImplementSimple<T> {
-  Node<T> root;
-
-}
