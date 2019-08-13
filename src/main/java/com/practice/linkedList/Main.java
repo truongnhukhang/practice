ac420d7f317a01170be3f66d851bf401d8bbc516
sicacho <trustme013@gmail.com>
2019-08-14 06:41:14 +0700

    keep learning

--- src/main/java/com/practice/linkedList/Main.java
+++ src/main/java/com/practice/linkedList/Main.java
@@ -4,13 +4,17 @@ public class Main {
   public static void main(String[] args) {
     LinkedListImplementSimple<String> implementSimple = new LinkedListImplementSimple<>();
     implementSimple.insert("A");
-//    implementSimple.insert("B");
-//    implementSimple.insert("C");
-//    implementSimple.insert("D");
-//    implementSimple.insert("E");
-//    implementSimple.insert("F");
-//    implementSimple.insert("G");
-//    implementSimple.insert("H");
+    implementSimple.insert("B");
+
+    implementSimple.reserve();
+    implementSimple.println();
+    implementSimple.insert("C");
+    implementSimple.insert("D");
+    implementSimple.insert("E");
+    implementSimple.insert("F");
+    implementSimple.insert("G");
+    implementSimple.insert("H");
+    implementSimple.println();
     implementSimple.reserve();
     implementSimple.println();
   }
