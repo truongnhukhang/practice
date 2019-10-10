9511371dddf1fff7bd9aeb6f34f09c0ce4a16540
sicacho <trustme013@gmail.com>
2019-10-10 23:31:02 +0700

    keep learning

---
+++ src/main/java/com/practice/leetcode/explorer/medium/TreesAndGraphs/BSTInorderTraversal.java
@@ -0,0 +1,38 @@
+package com.practice.leetcode.explorer.medium.TreesAndGraphs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BSTInorderTraversal {
+  public static void main(String[] args) {
+    TreeNode root = new TreeNode(2);
+    root.left = new TreeNode(1);
+    root.right = new TreeNode(10);
+    root.right.left = new TreeNode(3);
+    root.right.right = new TreeNode(15);
+    inorderTraversal(root).stream().forEach(System.out::println);
+  }
+
+  public static List<Integer> inorderTraversal(TreeNode root) {
+    List<Integer> result = new ArrayList<>();
+    if(root!=null) {
+      if(root.left!=null) {
+        inorderTraversal(root.left,result);
+      }
+      result.add(root.val);
+      if(root.right!=null) {
+        inorderTraversal(root.right,result);
+      }
+    }
+    return result;
+  }
+  public static void inorderTraversal(TreeNode root,List<Integer> list) {
+    if(root.left!=null) {
+      inorderTraversal(root.left,list);
+    }
+    list.add(root.val);
+    if(root.right!=null) {
+      inorderTraversal(root.right,list);
+    }
+  }
+}
