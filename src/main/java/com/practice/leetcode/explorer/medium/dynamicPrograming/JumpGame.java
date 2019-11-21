d3422c02567e1b7e6af7adf4b6fb53601454b2aa
sicacho <trustme013@gmail.com>
2019-11-22 06:12:10 +0700

    keep learning

--- src/main/java/com/practice/leetcode/explorer/medium/dynamicPrograming/JumpGame.java
+++ src/main/java/com/practice/leetcode/explorer/medium/dynamicPrograming/JumpGame.java
@@ -8,8 +8,9 @@ public class JumpGame {
   Map<String,Boolean> canJumpMap = new HashMap<>();
 
   public static void main(String[] args) {
-    int[] nums = {4,0,1,1,4,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+    int[] nums = {2,0,0,1,1,1,1,1,1,1,0,1};
     JumpGame jumpGame = new JumpGame();
+    System.out.println(jumpGame.canJumpMatrix(nums));
     System.out.println(jumpGame.canJump(nums));
   }
 
@@ -20,6 +21,59 @@ public class JumpGame {
       }
     }
     return false;
+  }
+
+  public boolean canJumpMatrix(int[] nums) {
+    if(nums.length==1) {
+      return true;
+    }
+    int[][] matrix = new int[nums.length][nums.length];
+    buildingMatrix(matrix,nums);
+    for (int i = 0; i < matrix.length; i++) {
+      System.out.println("");
+      for (int j = 0; j < matrix.length; j++) {
+        System.out.print(" " + matrix[i][j]);
+      }
+    }
+    System.out.println("");
+    for (int i = 0; i < matrix.length; i++) {
+      if(matrix[i][i]==0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public void buildingMatrix(int[][] matrix, int[] nums) {
+    for (int i = 0; i < matrix.length; i++) {
+      int jumpCount = nums[i];
+      if(i!=0) {
+        matrix[i][i] = matrix[i-1][i];
+      } else {
+        // we always can jump at first position
+        matrix[0][0] = 1;
+      }
+      for (int j = i+1; j < matrix.length; j++) {
+        // start Jumping
+        if(i-1>=0) {
+        // 1. If not the first jumping , can look the prv jumping to copy the farthest position
+          if(jumpCount > 0 || matrix[i-1][j]==1) {
+            jumpCount--;
+            matrix[i][j] = 1;
+          } else {
+            break;
+          }
+        } else {
+        // 2. We are the first one .
+          if(jumpCount > 0) {
+            matrix[i][j] = 1;
+            jumpCount--;
+          } else {
+            break;
+          }
+        }
+      }
+    }
   }
 
   public boolean canJump(int[] nums,int index,int maxIndex) {
