9f16c94a6ca172fd4860a0c4c2acdc6e5478e673
truongnhukhang <truongnhukhang1993@gmail.com>
2019-11-22 13:31:13 +0700

    keep learning

--- src/main/java/com/practice/leetcode/explorer/medium/dynamicPrograming/JumpGame.java
+++ src/main/java/com/practice/leetcode/explorer/medium/dynamicPrograming/JumpGame.java
@@ -8,10 +8,12 @@ public class JumpGame {
   Map<String,Boolean> canJumpMap = new HashMap<>();
 
   public static void main(String[] args) {
-    int[] nums = {2,0,0,1,1,1,1,1,1,1,0,1};
+    int[] nums = {1,1,2,2,0,1,1};
     JumpGame jumpGame = new JumpGame();
-    System.out.println(jumpGame.canJumpMatrix(nums));
-    System.out.println(jumpGame.canJump(nums));
+    System.out.println("Memory : " + jumpGame.canJumpMemory(nums));
+    System.out.println("Greedy : " + jumpGame.canJumpGreedy(nums));
+    System.out.println("Matrix : " + jumpGame.canJumpMatrix(nums));
+    System.out.println("DP : " + jumpGame.canJump(nums));
   }
 
   public boolean canJump(int[] nums) {
@@ -23,19 +25,59 @@ public class JumpGame {
     return false;
   }
 
+  public boolean canJumpGreedy(int[] nums) {
+    if(nums.length==1) {
+      return true;
+    }
+    int maxJumpIndex = nums[0];
+    int lastIndex = nums.length-1;
+    for (int i = 1; i < lastIndex; i++) {
+      if(i <= maxJumpIndex) {
+        if(nums[i]+i>maxJumpIndex) {
+          maxJumpIndex = nums[i] + i;
+        }
+      }
+    }
+    return maxJumpIndex >= lastIndex;
+  }
+
+  public boolean canJumpMemory(int[] nums) {
+    int[] memorize = new int[nums.length];
+    // we always jump at first steps;
+    memorize[0] = 1;
+    int length = nums.length;
+    for (int i = 0; i < length-1; i++) {
+      int jumps = nums[i];
+      int memCount = i+1;
+      while (jumps>0) {
+        if(memCount<memorize.length) {
+          memorize[memCount] = 1;
+        }
+        memCount++;
+        jumps--;
+      }
+    }
+    for (int i = 0; i < length; i++) {
+      if(memorize[i]==0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
   public boolean canJumpMatrix(int[] nums) {
     if(nums.length==1) {
       return true;
     }
     int[][] matrix = new int[nums.length][nums.length];
     buildingMatrix(matrix,nums);
-    for (int i = 0; i < matrix.length; i++) {
-      System.out.println("");
-      for (int j = 0; j < matrix.length; j++) {
-        System.out.print(" " + matrix[i][j]);
-      }
-    }
-    System.out.println("");
+//    for (int i = 0; i < matrix.length; i++) {
+//      System.out.println("");
+//      for (int j = 0; j < matrix.length; j++) {
+//        System.out.print(" " + matrix[i][j]);
+//      }
+//    }
+//    System.out.println("");
     for (int i = 0; i < matrix.length; i++) {
       if(matrix[i][i]==0) {
         return false;
