99173752b8a27150a0dd0879afb5cfd4e9d5a731
sicacho <trustme013@gmail.com>
2019-12-02 07:07:40 +0700

    keep learning

--- src/main/java/com/practice/leetcode/explorer/TwoCityScheduling.java
+++ src/main/java/com/practice/leetcode/explorer/TwoCityScheduling.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 public class TwoCityScheduling {
   public static void main(String[] args) {
     int[][] costs = {{70,311},{74,927},{732,711},{126,583},{857,118},{97,928},{975,843},{175,221},{284,929},{816,602},{689,863},{721,888}};
+//    int[][] costs = {{259,770},{448,54},{926,667},{184,139},{840,118},{577,469}};
     System.out.println((new TwoCityScheduling()).twoCitySchedCost(costs));
   }
 
@@ -26,34 +27,50 @@ public class TwoCityScheduling {
     Arrays.sort(costAs);
     Arrays.sort(costBs);
     int[] flag = new int[costsLen];
-    int numPer = costsLen;
-    int indexA = 0;
-    int indexB = 0;
+    int indexA = 0;int countA = costsLen/2;
+    int indexB = 0;int countB = costsLen/2;
     int sum = 0;
-    while (numPer>0) {
-      boolean pickA = false;
-      boolean pickB = false;
-      while (!pickA) {
-        Cost tempCostA = costAs[indexA];
+    while (countA >0 && countB > 0) {
+      Cost tempCostA = costAs[indexA];
+      Cost tempCostB = costBs[indexB];
+      if(tempCostA.cost < tempCostB.cost) {
+        tempCostA = costAs[indexA];
         if(flag[tempCostA.index]==0) {
-          pickA=true;
           flag[tempCostA.index] = 1;
+          System.out.println("Plus A : " + tempCostA.cost + " index : " +tempCostA.index);
           sum = sum + tempCostA.cost;
-          numPer--;
+          countA--;
         }
         indexA++;
       }
-      while (!pickB) {
-        Cost tempCostB = costBs[indexB];
+      if(tempCostA.cost >= tempCostB.cost){
+        tempCostB = costBs[indexB];
         if(flag[tempCostB.index]==0) {
           flag[tempCostB.index] = 1;
-          pickB=true;
+          countB--;
+          System.out.println("Plus B : " + tempCostB.cost + " index : " +tempCostB.index);
           sum = sum + tempCostB.cost;
-          numPer--;
         }
         indexB++;
       }
 
+    }
+    if(countA>0) {
+      for (int i = 0; i < costAs.length; i++) {
+        if(flag[costAs[i].index]==0) {
+          System.out.println("Plus A : " + costAs[i].cost + " index : " +costAs[i].index);
+          sum = sum + costAs[i].cost;
+        }
+      }
+    }
+
+    if(countB>0) {
+      for (int i = 0; i < costBs.length; i++) {
+        if(flag[costBs[i].index]==0) {
+          System.out.println("Plus B : " + costBs[i].cost + " index : " +costBs[i].index);
+          sum = sum + costBs[i].cost;
+        }
+      }
     }
     return sum;
   }
